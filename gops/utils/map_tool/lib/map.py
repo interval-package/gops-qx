2e0200ac444fd8888315a34dc351e2ac194fa7c4
yzy-yuzhouyang <2181046090@qq.com>
2024-12-29 20:58:22 +0800

    [hotfix] revise map drawing functions

--- gops/utils/map_tool/lib/map.py
+++ gops/utils/map_tool/lib/map.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
 import matplotlib.pyplot as plt
 
-
 import gops.utils.map_tool.proto.hdmap_pb2 as hdmap_pb2
 import google.protobuf.text_format as text_format
 from google.protobuf import json_format
@@ -24,11 +23,11 @@ class Map(MapBase):
 
     def load(self, map_file_name, attach_map_name=None):
         super().load(map_file_name)
-        # if attach_map_name is not None:
-        #     with open(attach_map_name, "r", encoding="utf-8") as f:
-        #         json_obj = json.load(f)
+    #     if attach_map_name is not None:
+    #         with open(attach_map_name, "r", encoding="utf-8") as f:
+    #             json_obj = json.load(f)
 
-        #     json_str = json.dumps(json_obj, indent=4)
+    #         json_str = json.dumps(json_obj, indent=4)
 
     def load_hd(self, traffic_map):
         self.map_pb = traffic_map.hdmap
@@ -63,8 +62,8 @@ class Map(MapBase):
         x_range = [np.inf, -np.inf]
         y_range = [np.inf, -np.inf]
         cnt = 1
-        for lane in self.map_pb.lanes:
-            if lane.type == "unknown":
+        for lane in self.map_pb["lanes"]:
+            if lane.get("type") == "unknown":
                 continue
             color_val = self.colors[cnt % len(self.colors)]
 
@@ -83,59 +82,60 @@ class Map(MapBase):
 
     def draw_link(self, is_show_link_ids=False):
         cnt = 1
-        for link in self.map_pb.links:
+        for link in self.map_pb["links"]:
             color_val = self.colors[cnt % len(self.colors)]
             self._draw_link_boundary(link, color_val)
 
     def draw_stopline(self, is_show_id=False):
         color_val = (1.0, 0.0, 0.0, 1.0)
-        for stopline in self.map_pb.stoplines:
+        for stopline in self.map_pb["stop_lines"]:
             self._draw_stopline(stopline, color_val)
 
     def draw_junction_shape(self, is_show_id=False):
         color_val = (0.0, 0.0, 1.0, 1.0)
-        for junction in self.map_pb.junctions:
+        for junction in self.map_pb["junctions"]:
             self._draw_junction_shape(junction, color_val)
             if is_show_id:
                 self._draw_junction_id(junction, color_val)
 
     def _draw_lane_id(self, lane, color_val):
         """draw lane id"""
-        if (len(lane.center_line) == 0):
+        if (len(lane["center_line"]) == 0):
             return
         x, y = self._find_lane_central_point(lane)
-        link_id = lane.link_id
+        link_id = lane["link_id"]
         link_idx = self.link2idx[link_id]
-        link = self.map_pb.links[link_idx]
-        segment_id = link.segment_id
+        link = self.map_pb["links"][link_idx]
+        segment_id = link["segment_id"]
 
-        id_str = "la:" + lane.lane_id + "\n" + "li:" + link_id + "\n" + "se:" + segment_id
-        self._draw_label(id_str, (x, y), color_val)
+        id_str = "la:" + lane["id"] + "\n" + "li:" + link_id + "\n" + "se:" + segment_id
+        # self._draw_label(id_str, (x, y), color_val)
 
     def _draw_link_id(self, link, color_val):
         x, y = self._find_link_boundary_central_point(link)
-        self._draw_label(link.link_id, (x, y), color_val)
+        # self._draw_label(link.link_id, (x, y), color_val)
 
     def _draw_junction_id(self, junction, color_val):
         """draw lane id"""
         px = []
         py = []
-        for p in junction.shape:
-            px.append(float(p.x))
-            py.append(float(p.y))
+        for p in junction["shape"]:
+            px.append(float(p["point"].get("x", 0)))
+            py.append(float(p["point"].get("y", 0)))
+
         if len(px) > 0:
             x = (np.min(px) + np.max(px)) / 2
             y = (np.min(py) + np.max(py)) / 2
 
-            junction_id = junction.junction_id
+            junction_id = junction["junction_id"]
             id_str = "junc:" + junction_id
-            self._draw_label(id_str, (x, y), color_val)
+            # self._draw_label(id_str, (x, y), color_val)
 
     @staticmethod
     def _find_lane_central_point(lane):
-        cp_idx = len(lane.center_line) // 2
+        cp_idx = len(lane["center_line"]) // 2
         # print(len(lane.center_line), cp_idx, lane.lane_id)
-        cp = lane.center_line[cp_idx]
+        cp = lane["center_line"][cp_idx]
         x = cp.get("point", {}).get("x", 0)
         y = cp.get("point", {}).get("y", 0)
         return x, y
@@ -144,9 +144,9 @@ class Map(MapBase):
     def _find_link_boundary_central_point(link):
         cp_idx = len(link.left_boundary) // 2
         # print(len(lane.center_line), cp_idx, lane.lane_id)
-        cp = link.left_boundary[cp_idx]
+        cp = link["left_boundary"][cp_idx]
 
-        return cp.x, cp.y
+        return cp["point"]["x"], cp["point"]["y"]
 
     @staticmethod
     def _draw_label(label_id, point, color_val):
@@ -176,9 +176,10 @@ class Map(MapBase):
     def _draw_stopline(stopline, color_val):
         px = []
         py = []
-        for p in stopline.shape:
-            px.append(float(p.x))
-            py.append(float(p.y))
+        for p in stopline["shape"]["points"]:
+            px.append(float(p.get("x", 0)))
+            py.append(float(p.get("y", 0)))
+
         plt.plot(px, py, ls="-", c=color_val, alpha=0.5, linewidth=5)
 
     @staticmethod
@@ -188,9 +189,11 @@ class Map(MapBase):
         for p in junction["shape"]:
             px.append(float(p["point"].get("x", 0)))
             py.append(float(p["point"].get("y", 0)))
+
         for p in junction["shape"]:
             px.append(float(p["point"].get("x", 0)))
             py.append(float(p["point"].get("y", 0)))
+
             break
         plt.plot(px, py, ls="-", c=color_val, alpha=0.5)
 
@@ -198,16 +201,18 @@ class Map(MapBase):
     def _draw_link_boundary(link, color_val):
         px = []
         py = []
-        for p in link.left_boundary:
-            px.append(float(p.x))
-            py.append(float(p.y))
+        for p in link["left_boundary"]:
+            px.append(float(p["point"].get("x", 0)))
+            py.append(float(p["point"].get("y", 0)))
+
         plt.plot(px, py, ls="-", c=color_val, alpha=0.5)
 
         px = []
         py = []
         for p in link.right_boundary:
-            px.append(float(p.x))
-            py.append(float(p.y))
+            px.append(float(p["point"].get("x", 0)))
+            py.append(float(p["point"].get("y", 0)))
+
         plt.plot(px, py, ls="-", c=color_val, alpha=0.5)
 
     @staticmethod
@@ -218,9 +223,9 @@ class Map(MapBase):
 
         px = []
         py = []
-        for p in lane.center_line:
-            px.append(float(p.x))
-            py.append(float(p.y))
+        for p in lane["center_line"]:
+            px.append(float(p["point"].get("x", 0)))
+            py.append(float(p["point"].get("y", 0)))
 
         if len(px) > 0:
             x_range[0] = np.min([x_range[0], np.min(np.array(px))])
@@ -236,15 +241,16 @@ class Map(MapBase):
         x, y = self._find_lane_central_point(jc)
         if jc.stop_line.has_stop_line:
             id_str = "sl:" + jc.stop_line.stop_line_id
-        self._draw_label(id_str, (x, y), color_val)
+        # self._draw_label(id_str, (x, y), color_val)
 
     def draw_connections(self):
-        for connection in self.map_pb.connections:
+        for connection in self.map_pb["connections"]:
             px = []
             py = []
-            for p in connection.path:
-                px.append(float(p.x))
-                py.append(float(p.y))
+            for p in connection["path"]["points"]:
+                px.append(float(p.get("x", 0)))
+                py.append(float(p.get("y", 0)))
+
             # plt.plot(px, py, ls=":", alpha=1, linewidth=2)
             plt.plot(px, py, ls="-", alpha=0.8, linewidth=2)
 
@@ -264,7 +270,7 @@ class Map(MapBase):
         scale = 0.6
 
         # Image size must be less than 2^16 in each direction.
-        max_width_height = 2**16
+        max_width_height = 2 ** 16
         if (x_range[1] - x_range[0]) * scale * dpi > max_width_height:
             scale = max_width_height / (x_range[1] - x_range[0]) / dpi - 0.01
         if (y_range[1] - y_range[0]) * scale * dpi > max_width_height:
@@ -279,8 +285,8 @@ class Map(MapBase):
         plt.axis("equal")
         plt.savefig("./" + map_name + ".pdf", dpi=2000)
         # plt.show()
-    
-    def draw_everything(self, show_id=False, show_link_boundary=False):
+
+    def draw_everything(self, show_id=False, show_link_boundary=False, map_name="map"):
         x_range, y_range = self.draw_lane(show_id)
         if show_link_boundary:
             self.draw_link(show_id)
@@ -296,7 +302,7 @@ class Map(MapBase):
         scale = 0.6
 
         # Image size must be less than 2^16 in each direction.
-        max_width_height = 2**16
+        max_width_height = 2 ** 16
         if (x_range[1] - x_range[0]) * scale * dpi > max_width_height:
             scale = max_width_height / (x_range[1] - x_range[0]) / dpi - 0.01
         if (y_range[1] - y_range[0]) * scale * dpi > max_width_height:
@@ -306,6 +312,7 @@ class Map(MapBase):
 
         # fig.set_figwidth(width)
         # fig.set_figheight(height)
+        plt.axis("equal")
+        plt.savefig("./" + map_name + ".pdf", dpi=2000)
 
-        plt.axis("equal")  
-        
+
